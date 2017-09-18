6dbe9f747dd00e24e9aa07def4d0e33cef4fa5e8
Andy Boyle <andy@sudostream.io>
2017-09-18 15:04:41 +0100

    test working

--- src/test/scala/io/sudostream/esandosreader/dao/TestMongoFindQueriesProxyStub.scala
+++ src/test/scala/io/sudostream/esandosreader/dao/TestMongoFindQueriesProxyStub.scala
@@ -1,10 +1,10 @@
 package io.sudostream.esandosreader.dao
 
+import io.sudostream.timetoteach.messages.scottish.{ScottishCurriculumAreaName, ScottishCurriculumLevel, ScottishEsAndOsMetadata, ScottishExperienceAndOutcome}
 import org.mongodb.scala.Document
 import org.mongodb.scala.bson.{BsonArray, BsonBoolean, BsonNull, BsonString}
 
 import scala.concurrent.Future
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class TestMongoFindQueriesProxyStub extends MongoFindQueriesProxy {
@@ -28,7 +28,7 @@ class TestMongoFindQueriesProxyStub extends MongoFindQueriesProxy {
         BsonString("FIRST"),
         BsonString("SECOND")
       ),
-      "curriculumAreaName" -> BsonString("EXPRESSIVE_ARTS"),
+      "curriculumAreaName" -> BsonString("Expressive Arts"),
       "eAndOSetName" -> None,
       "eAndOSetSectionName" -> BsonString("Participation in performances and presentations"),
       "eAndOSetSubSectionName" -> None,
@@ -36,8 +36,52 @@ class TestMongoFindQueriesProxyStub extends MongoFindQueriesProxy {
       "responsibilityOfAllPractitioners" -> BsonBoolean(true)
     )
 
+    val esAndOs2 = Document(
+      "experienceAndOutcome" -> BsonArray(
+        Document(
+          "sentence" -> BsonString("I have used the skills I have developed in the expressive arts to contribute to a " +
+            "public presentation/performance."),
+          "bulletPoints" -> BsonArray()
+        )
+      ),
+      "codes" -> BsonArray(
+        BsonString("EXA 3-01a")
+      ),
+      "curriculumLevels" -> BsonArray(
+        BsonString("THIRD")
+      ),
+      "curriculumAreaName" -> BsonString("Expressive Arts"),
+      "eAndOSetName" -> None,
+      "eAndOSetSectionName" -> BsonString("Participation in performances and presentations"),
+      "eAndOSetSubSectionName" -> None,
+      "eAndOSetSubSectionAuxiliaryText" -> None,
+      "responsibilityOfAllPractitioners" -> BsonBoolean(true)
+    )
+
+    val esAndOs3 = Document(
+      "experienceAndOutcome" -> BsonArray(
+        Document(
+          "sentence" -> BsonString("In everyday activity and play, I explore and make choices to develop my " +
+            "learning and interests. I am encouraged to use and share my experiences."),
+          "bulletPoints" -> BsonArray()
+        )
+      ),
+      "codes" -> BsonArray(
+        BsonString("HWB 0-19a")
+      ),
+      "curriculumLevels" -> BsonArray(
+        BsonString("EARLY")
+      ),
+      "curriculumAreaName" -> BsonString("Health And Wellbeing"),
+      "eAndOSetName" -> None,
+      "eAndOSetSectionName" -> BsonString("Planning for choices and changes"),
+      "eAndOSetSubSectionName" -> None,
+      "eAndOSetSubSectionAuxiliaryText" -> None,
+      "responsibilityOfAllPractitioners" -> BsonBoolean(true)
+    )
+
     Future {
-      List(esAndOs1)
+      List(esAndOs1, esAndOs2, esAndOs3)
     }
   }
 
